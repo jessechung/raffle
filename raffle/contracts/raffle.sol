f81073a070840628c5c9c8893a0a14e853c4db32
Jesse Chung <jessechung@put-75-206.reshall.berkeley.edu>
2018-02-28 00:07:12 -0800

    added constructor and raffle function to raffle.sol

---
+++ raffle/contracts/raffle.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.17;
+
+contract raffle {
+
+    address public owner;
+    uint public raffleEnd;
+    uint public itemPrice;
+    mapping(address => uint) rafflePot;
+
+    //initializes raffle and sets owner and time left in the raffle
+    function raffle(
+        address _owner,
+        uint _raffleEnd,
+        uint _itemPrice
+    ) public 
+    {
+        owner = _owner;
+        //sets the end time for raffle
+        raffleEnd = now + _raffleEnd;
+        itemPrice = _itemPrice;
+    }
+
+    //enter the raffle
+    function enterRaffle() public payable {
+        //requires the money sent to be equal to the itemPrice
+        require(msg.value == itemPrice);
+        //requires the raffle to still be ongoing
+        require(now <= raffleEnd);
+
+        rafflePot[msg.sender] += msg.value;
+    }
+
+    
+    
+}
